9a3a668ace6a6881e283ac731e28bc7bdbc00fe6
Brian Zhang <brianzhang@dyn-209-2-212-200.dyn.columbia.edu>
2014-04-05 18:50:46 -0400

    Updated style sheets

--- checkin.js
+++ checkin.js
@@ -4,3 +4,11 @@ authentication stuff
 
 ***************/
 
+
+
+function getCheckin() {
+    
+    window.location.replace("https://foursquare.com/oauth2/authenticate
+    ?client_id=YOUR_CLIENT_ID&response_type=code&redirect_uri=YOUR_REGISTERED_REDIRECT_URI");
+
+}
