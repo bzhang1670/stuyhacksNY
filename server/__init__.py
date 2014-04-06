17047f2d4a138c1bae8d6c39c19c41e40ce7ad8b
Shan Shi <sshi@thirtyfourth.(none)>
2014-04-06 03:19:25 -0400

    bnasdbflaksdjflkajsdlkvajsldfjlaksdjf

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, render_template, url_for, redirect, request
 
 app = Flask(__name__)
-user_name = 'Dan'
-chat_room_name = 'columbia'
-lol = "lol"
 
 @app.route("/")
 def home():
@@ -13,8 +10,8 @@ def home():
 @app.route('/chat', methods=['POST'])
 def submit():
 	if request.method == 'POST':
-		return request.form['user_name'] + " " + request.form['location']
-		#return render_template('chat.html', name=request.form['user_name'], loc=request.form['location'])
+		#return request.form['user_name'] + " " + request.form['location']
+		return render_template('chat.html', name=request.form['user_name'], loc=request.form['location'])
 
 if __name__ == '__main__':
 	app.run()
